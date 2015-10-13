d542d5f493d8d4971989693599f825c6264c9e5b
ZoltonZ12 <LavrovIV@yandex.ru>
2015-10-13 23:00:08 +0300

    first commit

--- 2_task/students_list.py
+++ 2_task/students_list.py
@@ -1,16 +1,25 @@
 
-lst = ['иванов','петров','сидоров','вавава','ываываыв','аываыва']
+lst = ['иван иванов','иван петров','вован сидоров','сергей жуков','василий жуков','ринь янь']
 ind = input('введите индекс студента:  ')
 
 if ind.isdigit():
-    print(lst[(int(ind)-1)])
+    print(lst[(int(ind))])
         
 else:
     print('должно быть целое число')
 
 srez = input('введите срез  студентов от  и до "от, до"')
 srez_list = srez.split(',')
-print(srez_list)
+
 print(lst[(int(srez_list[0])):(int(srez_list[1]))])
-        
+
+count_r = 0
+for i in lst:
+    if 'р' in i.split()[0]:
+        count_r +=  1
+    else:
+        pass
+print(count_r)
+
+
     
